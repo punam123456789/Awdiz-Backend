0fd4f6471f395f77d7e5da586cf60dffe1c16778
punam123456789 <punamp073@gmail.com>
2023-05-18 12:25:06 +0530

    updated

---
+++ controllers/All-Controllers.js
@@ -0,0 +1,15 @@
+export const Punam = (req,res) =>{
+    res.send("Sending message from Punam Funaction");
+}
+
+export const Kunal = (req,res) =>{
+    res.send("Sending message from Kunal Funaction");
+}
+
+export const Anu = (req,res) =>{
+    res.send("Sending message from anu Funaction");
+}
+
+export const Madhuri = (req,res) =>{
+    res.send("Sending message from Madhuri Funaction");
+}
--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("hello world");
-// console.log("hello world");
+import express from "express";
+import {Punam ,Kunal,Anu,Madhuri} from './controllers/All-Controllers.js';
+
+const app = express();
+app.get('/punam', Punam);
+app.get('/kunal', Kunal);
+app.get('/anu', Anu);
+app.get('/madhuri',Madhuri);
+
+// app.post();
+// app.patch();
+// app.put();
+// app.delete();
+
+
+
+
+app.listen(8000);//port
