c65d21abc4e401542509eb75c0b7bad0bf492988
punam123456789 <punamp073@gmail.com>
2023-05-19 18:17:29 +0530

    updated

--- controllers/All-Controllers.js
+++ controllers/All-Controllers.js
@@ -1,15 +1,20 @@
-export const Punam = (req,res) =>{
-    res.send("Sending message from Punam Funaction");
+export const kunal = (req, res) => {
+    res.send("Sending Message from Kunal Function");
 }
-
-export const Kunal = (req,res) =>{
-    res.send("Sending message from Kunal Funaction");
+export const Swaraj = (req, res) => {
+    try {
+        // throw new Error("I want to throw error..")
+        res.send('Sending Message from Swaraj Function')
+    } catch (error) {
+        console.log(error, "- error here")
+    }
 }
-
-export const Anu = (req,res) =>{
-    res.send("Sending message from anu Funaction");
+export const Anu = (req, res) => {
+    res.send("Sending Message from Anu Function")
 }
-
-export const Madhuri = (req,res) =>{
-    res.send("Sending message from Madhuri Funaction");
+export const Krishnan = (req, res) => {
+    res.send("Sending Message from Krishnan Function")
+}
+export const Abhi = (req, res) => {
+    res.send("Sending Message from Abhi Function")
 }
